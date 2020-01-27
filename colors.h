b24211d68bc550d07bcf2dc854d25f0cfde3afae
bambalic <lakiwow95@gmail.com>
2020-01-26 22:47:10 -0800

    added texture, added score and text, updated animation, added background

--- colors.h
+++ colors.h
@@ -12,9 +12,16 @@ extern GLfloat materialBrown1[];
 extern GLfloat materialBrown2[];
 extern GLfloat materialGray1[];
 extern GLfloat materialGreen1[];
+extern GLfloat materialGreen2[];
+extern GLfloat materialGreen3[];
 extern GLfloat materialRed1[];
 extern GLfloat materialPurple1[];
 extern GLfloat materialPurple2[];
+extern GLfloat materialBlue1[];
+extern GLfloat materialBlue2[];
+extern GLfloat materialBlue3[];
+extern GLfloat materialYellow1[];
+extern GLfloat materialOrange1[];
 
 
 
